aee24fa13591a63de63144b401ec0dd7eb9cf44d
Bo <bo.taylor.465@gmail.com>
2020-05-08 10:46:05 -0600

    downloads to local file, creates bs from local file, commented out code for createing bs object from page from internet, filters list, and saves to local csv. Ready for first attempt for grading. Still need to do written portion.

---
+++ python_code/us_census_beureau_scraper.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+import requests, csv, re, urllib.parse, os
+
+WEB_LINK = "http://www.census.gov/programs-surveys/popest.html"
+
+# download html file if not found locally
+if not os.path.isfile('./CurrentEstimates.html'):
+    try:
+        res = requests.get(WEB_LINK)
+        res.raise_for_status()
+    except Exception as exc:
+            print('There was a problem retrieving the page: %s' % (exc))
+    else:
+        html_document = open('CurrentEstimates.html', 'wb')
+        for chunk in res.iter_content(100_000):
+            html_document.write(chunk)
+        html_document.close()
+
+# create beautifulsoup object from local html document
+try:
+    html_soup = BeautifulSoup(open('./CurrentEstimates.html'), 'html.parser')
+except Exception as exc:
+    print('There was a problem retrieving the page: %s' % (exc))
+
+# # create beautifulsoup object with data from page
+# use this section to pull from web instead of local file
+# try:
+#     html_doc = requests.get(WEB_LINK)
+#     html_doc.raise_for_status()
+# except Exception as exc:
+#     print('There was a problem retrieving the page: %s' % (exc))
+# else:
+#     html_soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+# get all links and add them to a list
+list_of_links = []
+for link in html_soup.find_all('a'):
+    link = link.get('href')
+    link = urllib.parse.urljoin(WEB_LINK, link)     # convert relative to absolute URL's
+    list_of_links.append(link)
+
+html_regex = re.compile(r'(#.*$)')      # Search for links ending with '#anycharacters'
+filtered_links = []
+for link in list_of_links:
+    match_object = html_regex.search(link)
+    if match_object is None:            # If link doesn't end with '#something', then append
+        filtered_links.append(link)
+
+filtered_links = set(filtered_links)        # Remove duplicates
+filtered_links = sorted(filtered_links)     # Sort values
+
+output_file = open('output.csv', 'w', newline='')
+output_writer = csv.writer(output_file)
+for link in filtered_links:
+    # iterates over list to write each value in list to a new row instead of new column
+    output_writer.writerow([link])
+output_file.close()
