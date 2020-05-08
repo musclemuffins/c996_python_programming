14c7b9bea085e36378c24e59f8a3bffb14ee0f92
Bo <bo.taylor.465@gmail.com>
2020-05-08 10:57:52 -0600

    removed unnecessary code for grading

--- python_code/us_census_beureau_scraper.py
+++ python_code/us_census_beureau_scraper.py
@@ -3,6 +3,7 @@ import requests, csv, re, urllib.parse, os
 
 WEB_LINK = "http://www.census.gov/programs-surveys/popest.html"
 
+
 # download html file if not found locally
 if not os.path.isfile('./CurrentEstimates.html'):
     try:
@@ -20,17 +21,8 @@ if not os.path.isfile('./CurrentEstimates.html'):
 try:
     html_soup = BeautifulSoup(open('./CurrentEstimates.html'), 'html.parser')
 except Exception as exc:
-    print('There was a problem retrieving the page: %s' % (exc))
+    print('There was a problem createing the BeautifulSoup object: %s' % (exc))
 
-# # create beautifulsoup object with data from page
-# use this section to pull from web instead of local file
-# try:
-#     html_doc = requests.get(WEB_LINK)
-#     html_doc.raise_for_status()
-# except Exception as exc:
-#     print('There was a problem retrieving the page: %s' % (exc))
-# else:
-#     html_soup = BeautifulSoup(html_doc.text, 'html.parser')
 
 # get all links and add them to a list
 list_of_links = []
@@ -39,6 +31,7 @@ for link in html_soup.find_all('a'):
     link = urllib.parse.urljoin(WEB_LINK, link)     # convert relative to absolute URL's
     list_of_links.append(link)
 
+
 html_regex = re.compile(r'(#.*$)')      # Search for links ending with '#anycharacters'
 filtered_links = []
 for link in list_of_links:
@@ -46,8 +39,10 @@ for link in list_of_links:
     if match_object is None:            # If link doesn't end with '#something', then append
         filtered_links.append(link)
 
+
 filtered_links = set(filtered_links)        # Remove duplicates
 filtered_links = sorted(filtered_links)     # Sort values
+
 
 output_file = open('output.csv', 'w', newline='')
 output_writer = csv.writer(output_file)
